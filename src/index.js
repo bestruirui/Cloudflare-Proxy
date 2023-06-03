856b167900d981b45b06736719693b876ba9635c
小瑞 <1004279909@qq.com>
2023-06-03 19:02:33 +0000

    conf.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<App />}>
-          </Route>
-        </Routes>
-      </Router>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
+
+
